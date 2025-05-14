5e17c61ed70963cb784479c70bde0dc3df5d66c2
HelligeGiffel <114979429+HelligeGiffel@users.noreply.github.com>
2025-05-14 14:11:26 +0200

    Update app.py
    Added support for Dicts

--- Raspberry Pi Zero 2W/app.py
+++ Raspberry Pi Zero 2W/app.py
@@ -8,17 +8,19 @@ app = Flask(__name__)
 def receive_data():
     data = request.get_json()
     print("Received from ESP32:", data)
+    print("This is the raw data that was sent:")
+    print(data)
+    print(type(data))
     # create_new_employee(name, tagid, salary, otlimit, otform)
     # DB.create_new_employee("Bo", "0x53fcc401", 200, 5, 100)
     # Check to see if UID match anyone in the DB
     print(DB.fetch_all_temperatures())
-    uid = data["value"]
-    print(uid)
-    print(type(uid))
-    print(DB.convert_uid_to_name(uid))
+    print(data)
+    print(type(data))
+    # print(DB.convert_uid_to_name(uid))
 
     # Send back a simple response
-    response = {"message": "Hello ESP32, got your value: " + str(data['value'])}
+    response = {"message": "Hello ESP32, got your value: " + str(data)}
     return jsonify(response)
 
 if __name__ == '__main__':
